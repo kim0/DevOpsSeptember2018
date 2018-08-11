abb3f2fb7ce3a81f8ed8e024edbbd1aa09079df4
Ahmed Kamal <email.ahmedkamal@googlemail.com>
2018-08-11 21:43:37 +0000

    Add TF variables

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {}
+variable "location" {}
+variable "cluster_name" {}
+variable "dns_prefix" {}
+variable "ssh_public_key" {}
+variable "agent_count" {}
+variable "client_id" {}
+variable "client_secret" {}
